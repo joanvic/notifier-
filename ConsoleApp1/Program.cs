0974375ed853e8c85b7d717e8057c2a6425e0bb6
joanvic <117447073+joanvic@users.noreply.github.com>
2023-05-05 22:53:18 +0800

    changes

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ChatApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sender, receiver, message;
+            bool isContinuing = true;
+
+            while (isContinuing)
+            {
+                Console.Write("Choose if you are P1 or P2: ");
+                sender = Console.ReadLine();
+
+                Console.Write("Send Message: ");
+                message = Console.ReadLine();
+
+                Console.WriteLine("");
+                Console.WriteLine($"Message Sent\nYou sent a message on {DateTime.Now.ToString()}");
+
+                if (sender == "P1")
+                {
+                    Console.WriteLine("");
+                    Console.Write("Enter Message P2: ");
+                    receiver = "P2";
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.Write("Enter Message P1: ");
+                    receiver = "P1";
+                }
+
+                message = Console.ReadLine();
+                Console.WriteLine($"Message Received\n{receiver}: {message}");
+
+                Console.Write("Do you want to continue? (yes/no): ");
+                string continueResponse = Console.ReadLine();
+
+                if (continueResponse.ToLower() != "yes")
+                {
+                    isContinuing = false;
+                }
+            }
+
+            Console.WriteLine("Thanks for using!");
+        }
+    }
+}
